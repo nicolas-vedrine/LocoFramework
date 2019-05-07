dec1af79024d9a0f9fbf1500680aaa15207a0a8c
Nicolas VEDRINE <vedrinen@gmail.com>
2019-05-07 17:24:18 -0400

    finished our Loco FW

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by Developer on 07/05/2019.
+//  Copyright © 2019 Nicolas VEDRINE. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Developer on 07/05/2019.
+//  Copyright © 2019 Nicolas VEDRINE. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "did some stuff..."
+    }
+    
+}
